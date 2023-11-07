c4a701dbcf3ba6a61e5806704513c43b59ee726d
Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>
2023-11-07 09:53:24 +0100

    Use mock for network requests in tests
    Signed-off-by: Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>

--- Tests/BarbershopTagsAPIClientTests/BarbershopTagsAPIClientTests.swift
+++ Tests/BarbershopTagsAPIClientTests/BarbershopTagsAPIClientTests.swift
@@ -2,26 +2,51 @@ import XCTest
 @testable import BarbershopTagsAPIClient
 
 final class BarbershopTagsAPIClientTests: XCTestCase {
-    var client: BarbershopTagsAPIClient = .init(name: "Test Client")
     
     func testGetRequestFromParameters() throws {
-        let sut: Parameters = [
+        let sut = BarbershopTagsAPIClient(name: "Test Client")
+        
+        let parameters: Parameters = [
             .collection: TagInfo.Collection.classic
         ]
-        
-        let request = try client.getRequest(from: sut)
+                
+        let request = try sut.getRequest(from: parameters)
         
         XCTAssertNotNil(request.url)
         XCTAssertEqual(request.url?.query(percentEncoded: false), "Collection=classic")
     }
     
     func testRateWorngTag() async throws {
+        let sut = BarbershopTagsAPIClient(name: "TC", session: .init(mockResponder: RateWrongResponse.self))
         
         do {
-            try await client.rateTag(id: "123456", rating: 5)
+            try await sut.rateTag(id: "123456", rating: 5)
             XCTFail()
         } catch {
         }
-            
+        
+        RateWrongResponse.unregister()
+    }
+    
+    func testRateTagOK() async throws {
+        let sut = BarbershopTagsAPIClient(name: "TC", session: .init(mockResponder: RateOKResponse.self))
+
+        try await sut.rateTag(id: "123456", rating: 5)
+        
+        RateOKResponse.unregister()
     }
 }
+
+
+enum RateWrongResponse: MockURLResponder {
+    static func respond(to request: URLRequest) throws -> Data {
+        "tag 123456 does not exist".data(using: .utf8)!
+    }
+}
+
+enum RateOKResponse: MockURLResponder {
+    static func respond(to request: URLRequest) throws -> Data {
+        "ok".data(using: .utf8)!
+    }
+}
+
